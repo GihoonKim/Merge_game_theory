b6fb001b5cc1f2350543af222afa74ab5461c8ab
GihoonKim <rlgns4861@hanyang.ac.kr>
2018-08-13 10:05:37 +0900

    Prediction step revision

--- following.m
+++ following.m
@@ -1,4 +1,4 @@
-
+clear all; clc;
 %% Initialize
 car_m = [70, 0, 35, 0, 0, 0];  % X,Y,Vx,Vy,ax, ay
 car_t = [160, -10, 21.9, 0, 0, 0];
@@ -13,9 +13,9 @@ P_inm = 1/30*ones(5,3);    %manuever prob -> row : long,  column : lat
 P_int = 1/30*ones(5,3);    %need to be changed
 P_inb = 1/30*ones(5,3);
 
-P_inm(3,2)=1/2;   %constant velocity 
-P_int(3,2)=1/2;   %constant velocity
-P_inb(3,1)=1/2;   %constant velocity LCL
+P_inm(3,2)=16/30;   %constant velocity 
+P_int(3,2)=16/30;   %constant velocity
+P_inb(3,1)=16/30;   %constant velocity LCL
 
 % P_inm = 1/5*1/3*ones(5,3);    %manuever prob -> row : long,  column : lat
 % P_int = 1/5*1/3*ones(5,3);    %need to be changed
@@ -25,17 +25,17 @@ P_inb(3,1)=1/2;   %constant velocity LCL
 % given dy, ddy P(dy,ddy | class ) < -- how to modeling
 % fixed value 
 
-% interaction_unaware prob
+% interaction_unaware prob initialize
 P_uit = 1/5*1/3*ones(5,3);   
 P_uim = 1/5*1/3*ones(5,3);
 P_uib = 1/5*1/3*ones(5,3);
 
-% interaction_aware prob
+% interaction_aware prob initialize
 P_it = 1/5*1/3*ones(5,3);
 P_im = 1/5*1/3*ones(5,3);
 P_ib = 1/5*1/3*ones(5,3);
 
-P_b = 1/5*1/3*ones(5,3);
+P_b = 1/5*1/3*ones(5,3);    % bottom vehicle's maneuver prob initialize
 
 P_pred = 1/5*1/3*ones(5,3);
 P_pred_ex = 1/5*1/3*ones(5,3);
@@ -50,6 +50,7 @@ obj_str = gmdistribution([0,0],[1 0; 0 0.5],1);
 obj_right = gmdistribution([1,0.5],[1 0; 0 0.5],1);
 
 %connect_matrix = 
+%% %%%%%%%%%%%%%%%
 %% prediction step
 
 
@@ -96,18 +97,31 @@ P_uib = P_uib/sum(sum(P_uib));
 
 check = zeros(5,3);
 
+Num_v = 2; % top, bottom
+
 for i=1:5
     for j=1:3
         
         sum_temp = 0;
         %P_Ia_v_ = prod(prod(P_uit));
         
+%         for ti=1:5
+%             for tj=1:3
+%                 for mi=1:5
+%                     for mj=1:3
+%                         sum_temp = sum_temp + 1-risk([i,j],[ti,tj],[mi,mj],car_b,car_m,car_t)*P_uit(ti,tj)*P_uim(mi,mj);
+%                         % because of last two term, other vehicle can consider ego car's action(P_uim)
+%                     end
+%                 end
+%             end
+%         end
+
         for ti=1:5
             for tj=1:3
                 for mi=1:5
                     for mj=1:3
                         sum_temp = sum_temp + 1-risk([i,j],[ti,tj],[mi,mj],car_b,car_m,car_t)*P_uit(ti,tj)*P_uim(mi,mj);
-                        % because of last two term, other vehicle can consider ego car's action
+                        % because of last two term, other vehicle can consider ego car's action(P_uim)
                     end
                 end
             end
@@ -117,7 +131,8 @@ for i=1:5
         
         P_b(i,j) = P_uib(i,j)*P_ib(i,j);  %P(mj,v | pi_c)
         
-        P_pred(i,j) = prod(prod(P_b(i,j)*P_pred_ex));   % P_pred_ex value should be calculated or assumed 
+        P_pred(i,j) = P_b(i,j)*P_pred_ex(i,j);   % P_pred : given ego vehicle's trajectory, prob that bottom vehicle choose (i,j) maneuver.
+        %P(pt|pi_c_t-1) = p(m_t_j,v|pi_c_t-1)*P(pi_t-1_v | pi_c_t-2)
         
         check(i,j) = P_pred(i,j); 
         new_node = node('parents','child',P_pred(i,j));
@@ -125,7 +140,9 @@ for i=1:5
         node_list = [node_list, new_node];
     end    
 end
-
+%%%%%%%%%%%%%%%% 수정 필요 %%%%%%%%%%%%%%%%%%%
+%%%%%%%%  top, bottom 각각에 대한 고려 %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 check
 %% planning step
